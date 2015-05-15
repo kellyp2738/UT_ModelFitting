Random walk proposal distribution -- fixed proposal distribution calculation.

MCMC_multivariate_logscale_metropolis.py
@@ -632,15 +632,15 @@ def MCMC(prev_data, dir_name, burnin, iterations, pop_sizes, racc_pop, plot = Fa
                 post_log[i] = post_log[i-1]
                 t_eq_log[i] = t_eq_log[i-1]   
                 #step_size_log[i] = step_size_log[i-1]
-        
-    # get the covariance matrix for the proposal distribution... would be nice to have
-    # this outside the for-loop so the calculation didn't need to be repeated, but so it goes...
-    for_cov=iteration_log[50000+interval:100000,]
-    inflate=1.5
-    proposal_covariance=inflate*np.cov(for_cov, rowvar=0)
-    proposal_mean=np.mean(for_cov, axis=0)
             
-    for i in range(100000,iterations):   
+    for i in range(100000,iterations):
+
+	# get the covariance matrix for the proposal distribution
+	# proposal distribution is defined by ALL iterations after adaptive step size checking stops
+    	for_cov=iteration_log[50000+interval:i,]
+    	inflate=1.5
+    	proposal_covariance=inflate*np.cov(for_cov, rowvar=0)
+    	proposal_mean=np.mean(for_cov, axis=0)   
     
         # ----------------------------------------------------------------------------------------------
         # -- Subsequent iterations build the chain