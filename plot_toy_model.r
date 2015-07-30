0dea39dd2a94f64374b70ffcf1433ce6e6ff0abd
Kelly Anne Pierce <kellyannepierce@gmail.com>
2015-07-30 12:17:39 -0600

    Minor edits.

--- plot_toy_model.r
+++ plot_toy_model.r
@@ -1,4 +1,4 @@
-## Visualize results from the toy model
+## Visualize results from the toy model for tick borne disease transmission
 
 setwd('~/Desktop/UT_ModelFitting/')
 
@@ -6,14 +6,28 @@ ticks<-read.csv('toy_model_scenarios_tick_out.csv')
 deer<-read.csv('toy_model_scenarios_deer_out.csv')
 deerAB<-read.csv('toy_model_scenarios_deerAB_out.csv')
 
-par(mfrow=c(3,1), mar=c(5,12,2,2))
-plot(ticks$prefs, ticks$Scenario_1, type='l', xlab='', ylab='',
-     lty=3, cex.lab=1.5, cex.axis=1.5, las=1, ylim=c(0, 0.03), axes=FALSE)
+#par(mfrow=c(3,1), mar=c(5,12,2,2))
+pdf(file='~/Dropbox/EEID2015/Toy_Model_TickPrev_Motivation.pdf', height=15/2.54, width=15/2.54)
+par(mfrow=c(1,1), mar=c(5,6,2,2))
+plot(ticks$prefs, (ticks$Scenario_1)*100, type='l', xlab='Fraction of Ticks Feeding on Reservoir', ylab='',
+     #lty=3, 
+     col='#54278f', lwd=4,
+     cex.lab=1.5, cex.axis=1.5, las=1, ylim=c(0, 3), axes=FALSE)
 axis(side=1, cex.axis=1.5)
-axis(side=2, las=1, cex.axis=1.5, at=c(0, 0.01, 0.02, 0.03))
-mtext(text='Infection \nPrevalence \nin Ticks', side=2, line=5, cex=1.5)
-lines(ticks$prefs, ticks$Scenario_2, lty=4)
-lines(ticks$prefs, ticks$Scenario_3, lty=1)
+axis(side=2, las=1, cex.axis=1.5, at=c(0, 1, 2, 3))
+#mtext(text='Infection \nPrevalence \nin Ticks', side=2, line=5, cex=1.5)
+mtext(text='Infection Prevalence in Ticks (%)', side=2, line=2, cex=1.5)
+lines(ticks$prefs, (ticks$Scenario_2)*100, 
+      #lty=4)
+      col='#807dba', lwd=4)
+lines(ticks$prefs, (ticks$Scenario_3)*100, 
+      #lty=1)
+      col='#bcbddc', lwd=4)
+legend(x='topleft', legend=c('1:9', '3:7', '5:5'), 
+       cex=1.2, bty='n', title='Reservoir:Non-Reservoir',
+       #lty=c(1,4,3))
+       fill=c('#bcbddc', '#807dba', '#54278f'))
+dev.off()
 
 plot(deer$prefs, deer$Scenario_1, type='l', xlab='', ylab='', axes=FALSE,
      lty=3, cex.lab=1.5, cex.axis=1.5, las=1, ylim=c(0, 0.2))
